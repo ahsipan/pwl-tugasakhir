4ced1f91ac2e5e1b052b5ff629ab7ea82f930493
Nathanael Ivan Pratama <89458229+ahsipan@users.noreply.github.com>
2022-06-24 20:49:42 +0700

    yoww

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav></Nav>
+    <div class="auth-wrapper">
+      <div class="auth-inner">
+          <router-view/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
+  import Nav from './components/Nav.vue'
+  export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Nav,
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+body{
+  background: #ffffff !important;
+  min-height: 100vh;
+  display: flex;
+  font-weight: 400;
+}
+h1,h2,h3,h4,h5,h6,label,span{
+  font-weight: 500;
+}
+body, html, #app, #root, .auth-wrapper{
+  width: 100%;
+  height: 100%;
+}
+#app{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.navbar-light{
+  background-color: #1c8ef9;
+  box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+}
+.auth-wrapper{
+  margin-top: 50px;
+  justify-content: center;
+  flex-direction: column;
+  text-align: left;
+}
+.auth-inner{
+  display:flex;
+  margin: auto;
+  width: 450px;
+  padding: 40px 55px 45px 55px;
+  border-radius: 15px;
+}
+.auth-wrapper .form-control:focus{
+  border-color: #167bff;
+  box-shadow: none;
+}
+.auth-wrapper h3{
+  text-align: center;
+  margin: 0;
+  line-height: 1;
+  padding-bottom: 20px;
+}
+
+custom-control-label{
+  font-weight: 400;
 }
 </style>
